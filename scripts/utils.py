dd847596b54a955cd2750c03e6ea03ee917fc598
Moritz Gerster <45031224+moritz-gerster@users.noreply.github.com>
2023-10-25 01:02:06 +0200

    save_paths area

--- scripts/utils.py
+++ scripts/utils.py
@@ -1,7 +1,7 @@
-import numpy as np
-from pathlib import Path
 import os
+
 import netCDF4 as nc
+import numpy as np
 
 
 def _load_data(fname):
@@ -12,7 +12,7 @@ def _load_data(fname):
 
 
 def _load_lfp(data, brain_area):
-    brain_areas = data.variables["brain_area_lfp"][:]
+    brain_areas = _get_brain_areas(data)
     brain_area_idx = np.where(brain_areas == brain_area)[0][0]
     lfp_data = data.variables["lfp"]
     brain_area_lfp = lfp_data[brain_area_idx].data
@@ -25,3 +25,8 @@ def _load_spiking(data, brain_area):
     spiking_data = data.variables["spike_rate"]
     neural_spiking = spiking_data[brain_area_idx].data
     return neural_spiking
+
+
+def _get_brain_areas(data):
+    brain_areas = data.variables["brain_area_lfp"][:]
+    return brain_areas
